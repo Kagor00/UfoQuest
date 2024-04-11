815948c91aa317abaf80bfb110dd06cc22775980
Ihor Kolybaba <kagor00@gmail.com>
2024-04-11 19:53:01 +0300

    add dialogueTree

---
+++ src/main/java/com/javarush/kolybaba/ufoquest/servlets/LogicServlet.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import dialogueTree.QuestMatrix;
+import dialogueTree.UfoQuestMatrix;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/jsp/logic")
+public class LogicServlet extends HttpServlet {
+    private final QuestMatrix questMatrix = new UfoQuestMatrix();
+    private static final String SOURCE_PARAMETER = "source";
+    private static final String BUTTON_PARAMETER = "button";
+    private static final int JSP_PAGE_INDEX = 0;
+    private static final int VICTORIES_INCREMENT_INDEX = 1;
+    private static final int LOSSES_INCREMENT_INDEX = 2;
+    private static final String VICTORIES_ATTRIBUTE = "victories";
+    private static final String LOSSES_ATTRIBUTE = "losses";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer victoriesCount = getStats(req)[0];
+        Integer lossesCount = getStats(req)[1];
+        String source = req.getParameter(SOURCE_PARAMETER);
+        String button = req.getParameter(BUTTON_PARAMETER);
+        List<Object> formListInfo = questMatrix.getFormAttributes(source, button);
+        String jspPage = "/jsp/" + formListInfo.get(JSP_PAGE_INDEX);
+        victoriesCount += (Integer) formListInfo.get(VICTORIES_INCREMENT_INDEX);
+        lossesCount += (Integer) formListInfo.get(LOSSES_INCREMENT_INDEX);
+        session.setAttribute(VICTORIES_ATTRIBUTE, victoriesCount);
+        session.setAttribute(LOSSES_ATTRIBUTE, lossesCount);
+        req.getServletContext().getRequestDispatcher(jspPage).forward(req, resp);
+    }
+
+    private Integer[] getStats(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        Integer victoriesCount = (Integer) session.getAttribute(VICTORIES_ATTRIBUTE);
+        Integer lossesCount = (Integer) session.getAttribute(LOSSES_ATTRIBUTE);
+        if (victoriesCount == null) {
+            victoriesCount = 0;
+        }
+        if (lossesCount == null) {
+            lossesCount = 0;
+        }
+        return new Integer[]{victoriesCount, lossesCount};
+    }
+}
